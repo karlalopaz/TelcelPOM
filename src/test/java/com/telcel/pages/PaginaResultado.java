0f3ca2ad364e58327e7ffbbd6193ff76be4e3357
Karla Lopez del Castillo <karla.lopez2@astrazeneca.com>
2020-06-30 16:43:12 -0500

    agregando proyecto maven/project objects

---
+++ src/test/java/com/telcel/pages/PaginaResultado.java
@@ -0,0 +1,50 @@
+package com.telcel.pages;
+import com.telcel.utils.Celular;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PaginaResultado
+{
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    public PaginaResultado(WebDriver we) {
+        this.driver = we;
+        wait = new WebDriverWait(driver, 15);
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(css=".comp-telcel-mosaico-equipos")
+    WebElement listaResultados; //
+
+    @FindBy(css="p.telcel-mosaico-equipos-marca")
+    WebElement marca_ModeloCelular; //
+
+    @FindBy( css="p.telcel-mosaico-equipos-nombre-equipo")
+    WebElement nombreCelular; //
+
+    @FindBy(css="div.ng-scope > p.telcel-mosaico-equipos-precio")
+    WebElement precioFinal; //
+
+    @FindBy(css=".telcel-mosaico-equipos-capacidad-numero")
+    WebElement capacidadCelular; //
+
+    public void verificarPagina() {
+
+    }
+
+    public void verificarResultados() {
+
+    }
+
+    public Celular capturarInformacionCelular(int numCelular) {
+        return null;
+    }
+
+    public void seleccionarCelular(int numCelular) {
+
+    }
+}
